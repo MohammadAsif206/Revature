ca7bdfd36753b9c0434679e070eb783457d8138f
moham <mohammad.asif2008@hotmail.com>
2021-06-05 12:56:37 -0500

    updated, new functionalities added

---
+++ ASYNCJS/asyncd.md
@@ -0,0 +1,7 @@
+# Asynchronous
+- JS is single threaded lan
+- It can only Ever do one thing at a time
+    - other lan can make multiple thread and do several operations simultaenously
+- If you have some infinite JS loop your we page breaks
+- JS cannot maek threads but IS asynchronous
+    - JS is just an engine
--- DOM/outside.js
+++ DOM/outside.js
@@ -2,14 +2,39 @@
 //     alert("External JS");
 // }
 
-let calc = function(num1, num2, calcType) {
-    if (calcType === "add") {
-        return num1 + num2;
-    } else if (calcType === "multiply") {
-        return num2 * num1;
-    } else {
+// let calc = function(num1, num2, calcType) {
+//     if (calcType === "add") {
+//         return num1 + num2;
+//     } else if (calcType === "multiply") {
+//         return num2 * num1;
+//     } else {
 
-        return num1 / num2;
+//         return num1 / num2;
+//     }
+// }
+// console.log(calc(4, 5, "multiply"));
+
+var data = [{ "page": 1, "pages": 1, "per_page": "50", "total": 1 },
+    [{
+        "id": "AFG",
+        "iso2Code": "AF",
+        "name": "Afghanistan",
+        "region": { "id": "SAS", "iso2code": "8S", "value": "South Asia" },
+        "adminregion": { "id": "SAS", "iso2code": "8S", "value": "South Asia" },
+        "incomeLevel": { "id": "LIC", "iso2code": "XM", "value": "Low income" },
+        "lendingType": { "id": "IDX", "iso2code": "XI", "value": "IDA" },
+        "capitalCity": "Kabul",
+        "longitude": "69.1761",
+        "latitude": "34.5228"
+    }]
+];
+
+for (let p in data) {
+    //productName would be "laptop", "cellphone", etc.
+    //products[productName] would be an array of brand/price objects
+    var inner = data[p];
+    for (var i = 0; i < inner.length; i++) {
+        console.log(inner[i].id);
+        console.log(inner[i].name);
     }
 }
-console.log(calc(4, 5, "multiply"));
